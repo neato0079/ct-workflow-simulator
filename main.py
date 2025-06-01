563574c8f5cc7b2fe9c68eaecc70fa18b07c9782
Matt Q <neato0079@gmail.com>
2025-06-01 16:58:09 -0700

    add ct_tech class

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from datetime import time
 
+class CT_tech:
+    '''
+    efficiency weight: <int> # use this to subtract avg exam time from study type
+    avg non working time (aka slacking): <int>
+    '''
+    def __init__(self, efficiency_weight:int, lag_weight:int):
+        self.efficiency_weight = efficiency_weight
+        self.lag_weight = lag_weight
+
 class Siumulation:
     '''
     CT techs: <list of CT tech instance classes>
@@ -17,6 +26,7 @@ class Siumulation:
             scnr_asgnmnts:dict,
             trnsprt_vol:int,
             exam_vol:int,
+            avg_exam_dur:time, # this is temporary. studies should have their own class where duration is declared
             sim_dur:time
             ) -> None:
         
@@ -24,4 +34,5 @@ class Siumulation:
         self.scnr_asgnmnts = scnr_asgnmnts
         self.trnsprt_vol = trnsprt_vol
         self.exam_vol = exam_vol
+        self.avg_exam_dur = avg_exam_dur
         self.sim_dur = sim_dur
