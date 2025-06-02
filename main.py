6c44ce35ce4c049951ffdaaefb0263f63ee40dc8
Matt Q <neato0079@gmail.com>
2025-06-02 05:39:53 -0700

    add to CT_tech class description

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from simulator import Simulator
 class CT_tech:
     '''
     Efficiency weight: <float> 
-        - use this to reduce avg exam duration from study type
+        - use this to reduce avg exam duration 
         - ranges from 0.0 - 1.0
             0.0 = no efficiency so no change to exam duration
             1.0 = so efficeint that the exam duration is 0
-        - exam_dur * eff_wght = simulated exam duration
-    avg non working time (aka slacking): <int>
+
+    Lag weight: <float>
+        - use this to increase avg exam duration 
+        - ranges from 0.0 - 1.0
+            0.0 = no lag time so no change to exam duration
+            1.0 = so much lag that the exam duration doubles
     '''
     def __init__(self, eff_wght:float = 0, lag_weight:float = 0):
         self.eff_wght = eff_wght
         self.lag_weight = lag_weight
 
 
-
 if __name__ == "__main__":
-
 
     # set parameters for simulator class
     tech1 = CT_tech(0.1)
