dd0a70aa625b5b97c6c16af80a16b42fcf6afd0b
Matt Q <neato0079@gmail.com>
2025-06-01 17:51:26 -0700

    allow for calculation of simulated exam duration and highest n of exams completed for sim class

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from datetime import time
+from datetime import timedelta
 
 class CT_tech:
     '''
-    efficiency weight: <int> # use this to subtract avg exam time from study type
+    Efficiency weight: <float> 
+        - use this to reduce avg exam duration from study type
+        - ranges from 0.0 - 1.0
+            0.0 = no efficiency so no change to exam duration
+            1.0 = so efficeint that the exam duration is 0
+        - exam_dur * eff_wght = simulated exam duration
     avg non working time (aka slacking): <int>
     '''
-    def __init__(self, efficiency_weight:int, lag_weight:int):
-        self.efficiency_weight = efficiency_weight
+    def __init__(self, eff_wght:float = 0, lag_weight:float = 0):
+        self.eff_wght = eff_wght
         self.lag_weight = lag_weight
 
 class Siumulation:
@@ -22,12 +27,12 @@ class Siumulation:
     '''
     def __init__(
             self,
-            ct_techs:list[object],
+            ct_techs:list[CT_tech],
             scnr_asgnmnts:dict,
             trnsprt_vol:int,
             exam_vol:int,
-            avg_exam_dur:time, # this is temporary. studies should have their own class where duration is declared
-            sim_dur:time
+            avg_exam_dur:timedelta, # this is temporary. studies should have their own class where duration is declared
+            sim_dur:timedelta # seconds
             ) -> None:
         
         self.ct_techs = ct_techs
@@ -37,16 +42,64 @@ class Siumulation:
         self.avg_exam_dur = avg_exam_dur
         self.sim_dur = sim_dur
 
-    def avg_simulated_exam_dur(self):
+    def set_sim_exam_dur(self) -> timedelta:
         '''
         Given an exam volume, consider the CT techs' performance and scanner assignments and return the average simulated exam duration
         '''
+        self.simulated_exam_duration = self.avg_exam_dur
 
-    def most_exams_completed(self):
+        # loop through ct_techs and apply eff_wght to avg_exam_dur
+        for tech in self.ct_techs:
+            duration_reduction = self.simulated_exam_duration * tech.eff_wght
+            self.simulated_exam_duration = self.simulated_exam_duration - (duration_reduction)
+
+        # loop through ct_techs and apply lag_wght to avg_exam_dur
+        for tech in self.ct_techs:
+            duration_increase = self.simulated_exam_duration * tech.eff_wght
+            self.simulated_exam_duration = self.simulated_exam_duration + (duration_increase)
+
+        return self.simulated_exam_duration
+
+    def most_exams_completed(self) -> int:
         '''
         Given a simulation duration, consider the CT techs' performance and scanner assignments and return the highest number of exams completed
+        '''
+        self.set_sim_exam_dur()
+        return self.sim_dur // self.simulated_exam_duration
+    
+    def apply_transit(self):
+        '''
+        Consider transport times and add that to the exam duration
         '''
 
 
     def run(self):
         pass
+
+if __name__ == "__main__":
+
+
+    # set parameters for simulation
+    tech1 = CT_tech(0.1)
+    tech2 = CT_tech(0.3)
+    slow_tech = CT_tech(0, 0.2)
+    ct_techs = [tech1, slow_tech]
+
+    scanner_assignments = {
+        'ED': tech1,
+        'inpatient': tech2
+    }
+
+    transport_vol = 5
+
+    exam_vol = 40
+
+    avg_exam_dur = timedelta(seconds=420) # 7min
+
+    sim_dur = timedelta(hours=4)
+
+    sim1 = Siumulation(ct_techs=ct_techs, scnr_asgnmnts=scanner_assignments, trnsprt_vol=transport_vol, exam_vol=exam_vol, avg_exam_dur=avg_exam_dur, sim_dur=sim_dur)
+    print('avg exam time in seconds:')
+    print(sim1.set_sim_exam_dur())
+    print(f'total number of exams completed in {sim1.sim_dur} hours:')
+    print(sim1.most_exams_completed())
